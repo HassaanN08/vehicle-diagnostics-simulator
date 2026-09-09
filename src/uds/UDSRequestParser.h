5f3dc609af7a4486cc6bb4c8c701edd4b02eefcc
Hassaan Nadeem <222315448+HassaanN08@users.noreply.github.com>
2026-09-10 00:27:21 +0500

    Update the Unsupported Service NRC

--- src/uds/UDSRequestParser.h
+++ src/uds/UDSRequestParser.h
@@ -7,8 +7,9 @@
 #include "uds/UDSRequestProcessor.h"
 
 namespace NegativeResponses {
-    inline constexpr std::uint8_t notSupportedNRC { 0x11 };      //Requested Subfunction isn't supported
-    inline constexpr std::uint8_t incorrectLengthNRC { 0x13 };   //Incorrect Payload Length
+    inline constexpr std::uint8_t unSupportedFunctionNRC { 0x11 };      //Requested Subfunction isn't supported
+    inline constexpr std::uint8_t unSupportedServiceNRC { 0x12 };       //Requested Service isn't supported
+    inline constexpr std::uint8_t incorrectLengthNRC { 0x13 };          //Incorrect Payload Length
 }
 
 namespace UDSRequestParser {
@@ -16,46 +17,35 @@ namespace UDSRequestParser {
     inline std::vector<std::uint8_t> parser(ECU& ecu, const std::vector<std::uint8_t>& payload) {
 
         std::vector<std::uint8_t> responsePayload;
-        const std::uint8_t requestSID = payload[0];
-        const std::uint8_t positiveSID = (requestSID & 0xFF) + 0x40;
 
         if (payload.empty()) return responsePayload;
 
-        const size_t payloadLength {payload.size()};
+        const std::uint8_t requestSID { payload[0] };
+        const std::uint8_t positiveSID { static_cast<std::uint8_t>((requestSID & 0xFF) + 0x40) };
+
+        const size_t payloadLength { payload.size() };
 
         switch(requestSID) {
             case 0x10:
 
                 if (payloadLength == 2) {
-                    bool diagnosticSession {UDSRequestProcessor::setDiagnosticSessionControl(ecu, payload[1])};
+                    bool diagnosticSession { UDSRequestProcessor::setDiagnosticSessionControl(ecu, payload[1]) };
 
                     if (diagnosticSession) {
                         responsePayload = {positiveSID, payload[1]};
                     } else {
-                        responsePayload = {0x7F, requestSID, NegativeResponses::notSupportedNRC, payload[1]};
+                        responsePayload = {0x7F, requestSID, NegativeResponses::unSupportedFunctionNRC};
                     }
 
                 } else {
-                    responsePayload.push_back(0x7F);
-                    responsePayload.push_back(requestSID);
-                    responsePayload.push_back(NegativeResponses::incorrectLengthNRC);
-                    
-                    for (size_t i = 1; i < payloadLength; ++i) {
-                        responsePayload.push_back(payload[i]);
-                    }
+                    responsePayload = {0x7F, requestSID, NegativeResponses::incorrectLengthNRC};
                 }
 
                 break;
 
             default:
     
-                responsePayload.push_back(0x7F);
-                responsePayload.push_back(requestSID);
-                responsePayload.push_back(NegativeResponses::notSupportedNRC);
-                
-                for (size_t i = 1; i < payloadLength; ++i) {
-                    responsePayload.push_back(payload[i]);
-                }
+                responsePayload = {0x7F, requestSID, NegativeResponses::unSupportedServiceNRC};
         }
 
         return responsePayload;
--- tests/uds/UDSRequestParserTests.h
+++ tests/uds/UDSRequestParserTests.h
@@ -37,15 +37,21 @@ void udsRequestParserTests() {
 
     responsePayload = UDSRequestParser::parser(engine, {0x10, 0x03, 0x11});
 
-    assert(responsePayload[0] == 0x7F && responsePayload[1] == 0x10 && responsePayload[2] == 0x13 && responsePayload[3] == 0x03 && responsePayload[4] == 0x11);
+    assert(responsePayload[0] == 0x7F && responsePayload[1] == 0x10 && responsePayload[2] == 0x13);
 
-    responsePayload = UDSRequestParser::parser(engine, {0x12, 0x22});
+    responsePayload = UDSRequestParser::parser(engine, {0x17, 0x22});
 
-    assert(responsePayload[0] == 0x7F && responsePayload[1] == 0x12 && responsePayload[2] == 0x11 && responsePayload[3] == 0x22);
+    assert(responsePayload[0] == 0x7F && responsePayload[1] == 0x17 && responsePayload[2] == 0x12);
 
     responsePayload = UDSRequestParser::parser(brake, {0x10, 0x22});
 
-    assert(responsePayload[0] == 0x7F && responsePayload[1] == 0x10 && responsePayload[2] == 0x11 && responsePayload[3] == 0x22);
+    assert(responsePayload[0] == 0x7F && responsePayload[1] == 0x10 && responsePayload[2] == 0x11);
+
+    assert(brake.getCurrentDiagnosticSession() == DiagnosticSession::Default);
+
+    responsePayload = UDSRequestParser::parser(brake, {});
+
+    assert(responsePayload.empty());
 
     assert(brake.getCurrentDiagnosticSession() == DiagnosticSession::Default);
 }
